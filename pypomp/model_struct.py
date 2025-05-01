241ec2ba037bdcbccecb0c9ac410076a69b404cf
Aaron Abkemeier <aaronabkemeier@gmail.com>
2025-05-01 16:06:52 -0400

    Fix a few mistakes

--- pypomp/model_struct.py
+++ pypomp/model_struct.py
@@ -74,31 +74,30 @@ class dmeas:
     def __init__(self, struct):
         """
         Initializes the dmeas class with the required function structure.
-        Initializes the rproc class with the required function structure.
         While this function can check that the arguments of struct are in the 
         correct order, it cannot check that the output is correct. In this case,
         the user must make sure that struct returns a shape () JAX array.
 
         Args:
             struct (function): A function with a specific structure where the 
-                first three arguments must be 'y', 'params', and 'covars'.
+                first three arguments must be 'y', 'state', and 'params'.
 
         Raises:
             ValueError: If the first argument of the function is not 'y'.
-            ValueError: If the second argument of the function is not 'params'.
-            ValueError: If the third argument of the function is not 'covars'.
+            ValueError: If the second argument of the function is not 'state'.
+            ValueError: If the third argument of the function is not 'params'.
         """
 
         if struct.__code__.co_varnames[0] != "y":
             raise ValueError(
                 "The first argument of struct must be 'y'"
             )
-        if struct.__code__.co_varnames[1] != "params":
+        if struct.__code__.co_varnames[1] != "state":
             raise ValueError(
-                "The second argument of struct must be 'params'"
+                "The second argument of struct must be 'state'"
             )
-        if struct.__code__.co_varnames[2] != "covars":
+        if struct.__code__.co_varnames[2] != "params":
             raise ValueError(
-                "The third argument of struct must be 'covars'"
+                "The third argument of struct must be 'params'"
             )
         self.struct = struct

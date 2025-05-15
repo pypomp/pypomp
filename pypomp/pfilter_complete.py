647ae2038d9695e0d2e03e640950f2e8d0ba851c
Ed Ionides <ionides@umich.edu>
2025-05-15 10:26:53 -0400

    update and tidy for pfilter_complete

--- pypomp/pfilter_complete.py
+++ pypomp/pfilter_complete.py
@@ -1,3 +1,16 @@
+"""
+pfilter_complete is an adaptation of pfilter that can return states
+(i.e., values for each particle), filter mean, effective sample size.
+
+This is designed for diagnostics and other data analysis investigations.
+It is not designed for performance. Use pfilter within inference algorithms.
+
+At some future point, pfilter_complete might be incorporated within
+pfilter.
+
+Currently, pfilter_complete is somewhat experimental, reflecting the
+initial emphasis on methodology over data analysis for pypomp.
+"""
 import jax
 import numpy as np
 import jax.numpy as jnp
@@ -6,7 +19,8 @@ from functools import partial
 from pypomp.internal_functions import _normalize_weights, _resampler, _no_resampler
 
 def _pfilter_helper_complete(t, inputs, rprocess, dmeasure):
-    particlesF, theta, covars, loglik, norm_weights, counts, ys, thresh, key, logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr = inputs
+    [particlesF, theta, covars, loglik, norm_weights, counts, ys, thresh, key,
+        logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr] = inputs
     J = len(particlesF)
 
     if covars is not None and len(covars.shape) > 2:
@@ -30,34 +44,40 @@ def _pfilter_helper_complete(t, inputs, rprocess, dmeasure):
     norm_weights, loglik_t = _normalize_weights(weights)
     loglik += loglik_t
 
-    # **save loglik_t (conditional loglikelihood) 和 particlesP (states) **
+    # save loglik_t (conditional log-likelihood) particlesP (states) 
     logliks_arr = logliks_arr.at[t].set(loglik_t)  
     particles_arr = particles_arr.at[t].set(particlesP) 
 
-    # **calculate filtering mean (filt.mean) **
+    # calculate filtering mean (filt.mean) 
     # \sum (w_j * particle_j)
     filter_mean_t = (particlesP * jnp.exp(norm_weights[:,None])).sum(axis = 0)
     filter_mean_arr = filter_mean_arr.at[t].set(filter_mean_t)
 
-    # create filter.traj **
-     # variable "counts" records the source particle of particle i (1 - J) at time t from the previous step
+    # create filter.traj 
+    # variable "counts" records the source particle of particle i (1 - J) at
+    # time t from the previous step
     traj_arr = traj_arr.at[t].set(counts)
 
-    # **calculate effective sample size (ess) **
+    # calculate effective sample size (ess) 
     ess_t = 1.0 / jnp.sum(jnp.exp(norm_weights) ** 2)
     ess_arr = ess_arr.at[t].set(ess_t)
 
+    key, subkey = jax.random.split(key)
     oddr = jnp.exp(jnp.max(norm_weights)) / jnp.exp(jnp.min(norm_weights))
-    counts, particlesF, norm_weights = jax.lax.cond(oddr > thresh,
-                                                    _resampler,
-                                                    _no_resampler,
-                                                    counts, particlesP, norm_weights)
+    counts, particlesF, norm_weights = jax.lax.cond(
+        oddr > thresh,
+        _resampler,
+        _no_resampler,
+        counts, particlesP, norm_weights, subkey)
 
-    return [particlesF, theta, covars, loglik, norm_weights, counts, ys, thresh, key, logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr]
+    return [particlesF, theta, covars, loglik, norm_weights,
+        counts, ys, thresh, key,
+        logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr]
 
 
-def _pfilter_internal_complete(theta, ys, J, rinit, rprocess, dmeasure, covars=None, thresh=100, 
-                      key=None):
+def _pfilter_internal_complete(
+        theta, ys, J, rinit, rprocess, dmeasure, covars=None, thresh=100, 
+        key=None):
   
     if key is None:
         key = jax.random.PRNGKey(np.random.choice(int(1e18)))
@@ -68,41 +88,52 @@ def _pfilter_internal_complete(theta, ys, J, rinit, rprocess, dmeasure, covars=N
     counts = jnp.ones(J).astype(int)
     loglik = 0
 
-    logliks_arr = jnp.zeros(len(ys))  # save loglik_t (conditional loglikelihood)
+    logliks_arr = jnp.zeros(len(ys))  # save loglik_t (conditional log-likelihood)
     particles_arr = jnp.zeros((len(ys), J, particlesF.shape[-1])) # save states
     filter_mean_arr = jnp.zeros((len(ys), particlesF.shape[-1]))
     ess_arr = jnp.zeros(len(ys))
     traj_arr = jnp.zeros((len(ys), J))
 
     pfilter_helper_2 = partial(_pfilter_helper_complete, rprocess=rprocess, dmeasure=dmeasure)
-    particlesF, theta, covars, loglik, norm_weights, counts, ys, thresh, key, logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr = jax.lax.fori_loop(
+    [particlesF, theta, covars, loglik, norm_weights, counts, ys, thresh, key,
+        logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr] = jax.lax.fori_loop(
         lower=0, upper=len(ys), body_fun=pfilter_helper_2,
-        init_val=[particlesF, theta, covars, loglik, norm_weights, counts, ys, thresh, key, logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr])
+        init_val=[particlesF, theta, covars, loglik,
+            norm_weights, counts, ys, thresh, key,
+            logliks_arr, particles_arr, filter_mean_arr, ess_arr, traj_arr])
     
     # filt.t:
     b = jax.random.choice(key, jnp.arange(J), p=jnp.exp(norm_weights))
     filt_traj = jnp.zeros((len(ys), particlesF.shape[-1]))
     for t in range(len(ys)-1, -1, -1):
-        filt_traj = filt_traj.at[t].set(particles_arr[t, b])  # extract the state value corresponding the b th particle at time t and plug it into filt_traj
-        b = traj_arr[t, b] # find the corresponding particle from the previous step from traj_arr and update b
+        # extract the state value corresponding the b th particle at time t
+        # and plug it into filt_traj
+        filt_traj = filt_traj.at[t].set(particles_arr[t, b])
+        # find the corresponding particle from the previous step from traj_arr
+        # and update b
+        b = traj_arr[t, b] 
         b = jnp.int32(b)
     
-    # return 1. negative loglikelihood, 
-    # 2. mean loglikelihood, 
-    # 3. conditional loglikelihood 
+    # return 1. negative log-likelihood, 
+    # 2. mean log-likelihood, 
+    # 3. conditional log-likelihood 
     # 4. save states 
     # 5. filter mean
     # 6. effective sample size 
     # 7. filter_traj
-    return -loglik, -loglik/len(ys), logliks_arr, particles_arr, filter_mean_arr, ess_arr, filt_traj
+    return [-loglik, -loglik/len(ys), logliks_arr, particles_arr, filter_mean_arr,
+        ess_arr, filt_traj]
 
-def pfilter_complete(pomp_object=None, J=50, rinit=None, rprocess=None, dmeasure=None, theta=None, 
-            ys=None, covars=None, thresh=100, key=None):
+def pfilter_complete(
+        pomp_object=None, J=50,
+        rinit=None, rprocess=None, dmeasure=None, theta=None, 
+        ys=None, covars=None, thresh=100, key=None):
     if pomp_object is not None:
         return pomp_object.pfilter(J, thresh, key)
     elif rinit is not None and rprocess is not None and dmeasure is not None and theta \
         is not None and ys is not None:
-        return _pfilter_internal_complete(theta, ys, J, rinit, rprocess, dmeasure, covars, thresh, key)
+        return _pfilter_internal_complete(
+            theta, ys, J, rinit, rprocess, dmeasure, covars, thresh, key)
     else:
         raise ValueError("Invalid Arguments Input")
 

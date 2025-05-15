670048f8a8511a49ed741b19e4c3852120405252
Aaron Abkemeier <aaronabkemeier@gmail.com>
2025-05-15 09:59:19 -0400

    Clean up test_pfilter.py

--- test/test_pfilter.py
+++ test/test_pfilter.py
@@ -1,41 +1,47 @@
-import os
 import jax
-import sys
 import unittest
 import jax.numpy as jnp
 
-from tqdm import tqdm
-from pypomp.LG import *
+from pypomp.LG import LG
 from pypomp.pfilter import pfilter
 
-LG_obj = LG()
-ys = LG_obj.ys
-theta = LG_obj.theta
-covars = LG_obj.covars
-rinit = LG_obj.rinit
-rprocess = LG_obj.rprocess
-dmeasure = LG_obj.dmeasure
-rprocesses = LG_obj.rprocesses
-dmeasures = LG_obj.dmeasures
+
 
 class TestPfilter_LG(unittest.TestCase):
     def setUp(self):
-        self.key = jax.random.PRNGKey(111)
+        self.LG = LG()
+        self.key = jax.random.key(111)
         self.J = 5
-        self.ys = ys
-        self.theta = theta
-        self.covars = covars
-        
-        self.rinit = rinit
-        self.rprocess = rprocess
-        self.dmeasure = dmeasure
-        self.rprocesses = rprocesses
-        self.dmeasures = dmeasures
+        self.ys = self.LG.ys
+        self.theta = self.LG.theta
+        self.covars = self.LG.covars
+
+        self.rinit = self.LG.rinit
+        self.rprocess = self.LG.rprocess
+        self.dmeasure = self.LG.dmeasure
+        self.rprocesses = self.LG.rprocesses
+        self.dmeasures = self.LG.dmeasures
 
     def test_internal_basic(self):
-        val1 = pfilter(J=self.J, rinit=self.rinit, rprocess=self.rprocess, dmeasure=self.dmeasure, theta=self.theta,
-                       ys=self.ys, covars=self.covars, thresh=10, key=self.key)
-        val2 = pfilter(rinit=self.rinit, rprocess=self.rprocess, dmeasure=self.dmeasure, theta=self.theta, ys=self.ys, key=self.key)
+        val1 = pfilter(
+            J=self.J,
+            rinit=self.rinit,
+            rprocess=self.rprocess,
+            dmeasure=self.dmeasure,
+            theta=self.theta,
+            ys=self.ys,
+            covars=self.covars,
+            thresh=10,
+            key=self.key,
+        )
+        val2 = pfilter(
+            rinit=self.rinit,
+            rprocess=self.rprocess,
+            dmeasure=self.dmeasure,
+            theta=self.theta,
+            ys=self.ys,
+            key=self.key,
+        )
         self.assertEqual(val1.shape, ())
         self.assertTrue(jnp.isfinite(val1.item()))
         self.assertEqual(val1.dtype, jnp.float32)
@@ -44,9 +50,18 @@ class TestPfilter_LG(unittest.TestCase):
         self.assertEqual(val2.dtype, jnp.float32)
 
     def test_class_basic(self):
-        val1 = pfilter(LG_obj, self.J, thresh=10, key=self.key)
-        val2 = pfilter(LG_obj, key=self.key)
-        val3 = pfilter(LG_obj, self.J, self.rinit, self.rprocess, self.dmeasure, theta=[], ys=[], key=self.key)
+        val1 = pfilter(self.LG, self.J, thresh=10, key=self.key)
+        val2 = pfilter(self.LG, key=self.key)
+        val3 = pfilter(
+            self.LG,
+            self.J,
+            self.rinit,
+            self.rprocess,
+            self.dmeasure,
+            theta=[],
+            ys=[],
+            key=self.key,
+        )
         self.assertEqual(val1.shape, ())
         self.assertTrue(jnp.isfinite(val1.item()))
         self.assertEqual(val1.dtype, jnp.float32)
@@ -58,41 +73,40 @@ class TestPfilter_LG(unittest.TestCase):
         self.assertEqual(val3.dtype, jnp.float32)
 
     def test_invalid_input(self):
-        with self.assertRaises(ValueError) as text:
-            pfilter(key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
-
-        with self.assertRaises(ValueError) as text:
-            pfilter(J=self.J, key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
-
-        with self.assertRaises(ValueError) as text:
-            pfilter(J=self.J, thresh=-1, key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
-
-        with self.assertRaises(ValueError) as text:
-            pfilter(J=self.J, theta=self.theta, ys=self.ys, key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
-
-        with self.assertRaises(ValueError) as text:
-            pfilter(J=self.J, rinit=self.rinit, rprocess=self.rprocess, dmeasure=self.dmeasure, key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
-
-        with self.assertRaises(ValueError) as text:
-            pfilter(J=self.J, rinit=self.rinit, rprocess=self.rprocess, dmeasure=self.dmeasure, ys=self.ys, key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
-
-        with self.assertRaises(ValueError) as text:
-            pfilter(J=self.J, rinit=self.rinit, rprocess=self.rprocess, dmeasure=self.dmeasure, theta=self.theta, key=self.key)
-
-        self.assertEqual(str(text.exception), "Invalid Arguments Input")
+        arguments = [
+            {"key": self.key},
+            {"J": self.J, "key": self.key},
+            {"J": self.J, "thresh": -1, "key": self.key},
+            {"J": self.J, "theta": self.theta, "ys": self.ys, "key": self.key},
+            {
+                "J": self.J,
+                "rinit": self.rinit,
+                "rprocess": self.rprocess,
+                "dmeasure": self.dmeasure,
+                "key": self.key,
+            },
+            {
+                "J": self.J,
+                "rinit": self.rinit,
+                "rprocess": self.rprocess,
+                "dmeasure": self.dmeasure,
+                "ys": self.ys,
+                "key": self.key,
+            },
+            {
+                "J": self.J,
+                "rinit": self.rinit,
+                "rprocess": self.rprocess,
+                "dmeasure": self.dmeasure,
+                "theta": self.theta,
+                "key": self.key,
+            },
+        ]
+        for arg in arguments:
+            with self.assertRaises(ValueError) as text:
+                pfilter(**arg)
+            self.assertEqual(str(text.exception), "Invalid Arguments Input")
 
 
 if __name__ == "__main__":
-    unittest.main(argv=[''], verbosity=2, exit=False)
+    unittest.main(argv=[""], verbosity=2, exit=False)

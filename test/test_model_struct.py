943a751bdfac495d349cc7a7fb67b1ac90299113
Aaron Abkemeier <aaronabkemeier@gmail.com>
2025-05-17 18:24:02 -0400

    Add test_model_struct.py

---
+++ test/test_model_struct.py
@@ -0,0 +1,44 @@
+import unittest
+import pypomp as pp
+import jax.numpy as jnp
+
+
+class TestModelStruct(unittest.TestCase):
+    def test_RInit_value_error(self):
+        # Test that an error is thrown with incorrect arguments
+        with self.assertRaises(ValueError):
+            pp.RInit(lambda foo, J, covars: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.RInit(lambda params, foo, covars: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.RInit(lambda params, J, foo: jnp.array([0]))
+        # Test that correct arguments run without error
+        pp.RInit(lambda params, J, covars: jnp.array([0]))
+
+    def test_RProc_value_error(self):
+        # Test that an error is thrown with incorrect arguments
+        with self.assertRaises(ValueError):
+            pp.RProc(lambda foo, params, key, covars: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.RProc(lambda state, foo, key, covars: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.RProc(lambda state, params, foo, covars: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.RProc(lambda state, params, key, foo: jnp.array([0]))
+        # Test that correct arguments run without error
+        pp.RProc(lambda state, params, key, covars: jnp.array([0]))
+
+    def test_DMeas_value_error(self):
+        # Test that an error is thrown with incorrect arguments
+        with self.assertRaises(ValueError):
+            pp.DMeas(lambda foo, state, params: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.DMeas(lambda y, foo, params: jnp.array([0]))
+        with self.assertRaises(ValueError):
+            pp.DMeas(lambda y, state, foo: jnp.array([0]))
+        # Test that correct arguments run without error
+        pp.DMeas(lambda y, state, params: jnp.array([0]))
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[""], verbosity=2, exit=False)
--- test/test_util.py
+++ test/test_util.py
@@ -1,4 +1,3 @@
-import jax
 import unittest
 import jax.numpy as jnp
 import pypomp as pp
@@ -9,8 +8,8 @@ import pypomp as pp
 # library(pomp)
 # x = c(100,101,102,103,104)
 # logmeanexp(x,se=TRUE)
-#         est          se 
-# 102.8424765   0.7510094 
+#         est          se
+# 102.8424765   0.7510094
 #
 # import jax.numpy as jnp
 # import pypomp as pp
@@ -22,22 +21,19 @@ import pypomp as pp
 # >>> pp.logmeanexp_se(x)
 # Array(0.7510107, dtype=float32)
 
+
 class TestUtil(unittest.TestCase):
     def setUp(self):
         self.logmeanexp = pp.logmeanexp
         self.logmeanexp_se = pp.logmeanexp_se
-        self.x = jnp.array([100,101,102,103,104])
-
+        self.x = jnp.array([100, 101, 102, 103, 104])
 
     def test_val(self):
-       lme = self.logmeanexp(self.x)
-       lme_se = self.logmeanexp_se(self.x)
-       self.assertEqual(jnp.round(lme,2), 102.84)
-       self.assertEqual(jnp.round(lme_se,2), 0.75)
+        lme = self.logmeanexp(self.x)
+        lme_se = self.logmeanexp_se(self.x)
+        self.assertEqual(jnp.round(lme, 2), 102.84)
+        self.assertEqual(jnp.round(lme_se, 2), 0.75)
 
 
 if __name__ == "__main__":
     unittest.main(argv=[""], verbosity=2, exit=False)
-
-
-    

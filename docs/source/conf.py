8b1fb36f880cdd58e9470585cb02ffe22c091351
Aaron Abkemeier <aaronabkemeier@gmail.com>
2026-02-07 15:48:26 -0500

    0.4.3 Increment version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "pypomp"
 copyright = "2025, pypomp team"
 author = "pypomp team"
-release = "0.4.2"
+release = "0.4.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
